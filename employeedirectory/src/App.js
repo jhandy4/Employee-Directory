c6377d6686eaf52547688413cb864d33b1e4c4da
jhandy4 <58635628+jhandy4@users.noreply.github.com>
2020-06-07 15:56:38 -0400

    create app

--- employeedirectory/src/App.js
+++ employeedirectory/src/App.js
@@ -1,26 +1,144 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {useTable, useSortBy, useFilters} from "react-table";
+import {useState} from "react";
+import styled from "styled-components";
+import data from "./data.json";
 
-function App() {
+const Styles = styled.div `
+  table {
+    width: 100%;
+    border-spacing: 0;
+    border: 1px solid black;
+    tr {
+      :last-child {
+        td {
+          border-bottom: 0;
+        }
+      }
+    }
+    th,
+    td {
+      margin: 0;
+      padding: 1rem;
+      border-bottom: 1px solid black;
+      border-right: 1px solid black;
+      :last-child {
+        border-right: 0;
+      }
+    }
+  }
+`
+
+function Table({ columns, data }) {
+  const [filterInput, setFilterInput] = useState("");
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    setFilter
+  } = useTable(
+    {
+      columns,
+      data,
+    },
+    useFilters,
+    useSortBy,
+
+  )
+  const handleFilterChange = e => {
+    const value = e.target.value || undefined;
+    setFilter("firstName", value);
+    // setFilter("lastName", value); // Update the show.name filter. Now our table will filter and show only the rows which have a matching value
+    setFilterInput(value);
+  };
+  
+
+  // We don't want to render all 2000 rows for this example, so cap
+  // it at 20 for this use case
+  const firstPageRows = rows.slice(0, 20)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <input
+  value={filterInput}
+  onChange={handleFilterChange}
+  placeholder={"Search name"}
+/>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                // Add the sorting props to control sorting. For this example
+                // we can add them into the header props
+                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render('Header')}
+                  {/* Add a sort direction indicator */}
+                  <span>
+                    {column.isSorted
+                      ? column.isSortedDesc
+                        ? ' 🔽'
+                        : ' 🔼'
+                      : ''}
+                  </span>
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {firstPageRows.map(
+            (row, i) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map(cell => {
+                    return (
+                      <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                    )
+                  })}
+                </tr>
+              )}
+          )}
+        </tbody>
+      </table>
+    </>
+  )
 }
 
-export default App;
+function App() {
+
+  const columns = [
+    {
+      Header: '#',
+      accessor: 'id'
+    }, {
+      Header: 'First Name',
+      accessor: 'firstName'
+    }, {
+      Header: 'Last Name',
+      accessor: 'lastName'
+    }, {
+      Header: 'Email',
+      accessor: 'email'
+    },{
+      Header: 'Department',
+      accessor: 'department'
+    }
+  ]
+  
+
+  return (
+    <Styles>
+      <Table
+        data={data}
+        columns={columns}
+      />
+    </Styles>
+  )
+
+}
+
+export default App
